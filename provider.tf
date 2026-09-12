4e9f83a37991d91c9d67017f271211095070e18b
YosraMamdouh <Yousra.Mamdouh2400949@compit.aun.edu.eg>
2026-09-12 13:27:49 +0300

    Refactor provider.tf to use variables for configuration

--- provider.tf
+++ provider.tf
@@ -1,13 +1,36 @@
-provider "aws" {
-  region     = "us-east-1"
-  access_key = "test"
-  secret_key = "test"
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
 
+variable "floci_endpoint" {
+  type    = string
+  default = "http://localhost:4566"
+}
+
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = var.aws_region
+  access_key                  = "test"
+  secret_key                  = "test"
   skip_credentials_validation = true
-  skip_requesting_account_id  = true
   skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_requesting_account_id  = true
+  s3_use_path_style           = true
 
   endpoints {
-    ec2 = "http://localhost:4566"
+    ec2 = var.floci_endpoint
+    sts = var.floci_endpoint
+    s3  = var.floci_endpoint
   }
 }
